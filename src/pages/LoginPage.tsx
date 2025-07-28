d74c22b8a510cac458beca6175d020c2384bd324
namoradadomiguel79-ai <namoradadomiguel79@gmail.com>
2025-07-28 19:40:40 -0300

    Updated LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,9 @@
 @@ .. @@
  export const LoginPage: React.FC<LoginPageProps> = ({ onPasswordCorrect }) => {
    const [password, setPassword] = useState('');
+ }
 -  const correctPassword = 'miguel18'; // Defina a senha desejada
 +  const correctPassword = '640'; // Defina a senha desejada
  
    const handleSubmit = (e: React.FormEvent) => {
+   }
